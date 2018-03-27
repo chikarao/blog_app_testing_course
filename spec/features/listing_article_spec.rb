add473ae034ce3ce11c48adea12c8fc785a881fc
Chikara Okada <chikara@gmail.com>
2018-03-27 17:43:13 +0900

    Show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -4,10 +4,35 @@ RSpec.feature "Listing Articles" do
   #create two articles to disply
   #expect both title and bodies to be present
   before do
-    john = User.create!(email: "john@example.com", password: "password")
+    @john = User.create!(email: "john@example.com", password: "password")
     # login_as(john) don'T need to be looged in
-    @article1 = Article.create(title: "The first article", body: "Here is the body for the first article", user: john)
-    @article2 = Article.create(title: "The second article", body: "Here is the body for the second article", user: john)
+    @article1 = Article.create(title: "The first article", body: "Here is the body for the first article", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Here is the body for the second article", user: @john)
+  end
+
+  scenario "With articles created and user NOT signed in" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "With articles created and user IS signed in" do
+    login_as(@john)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
   end
 
   scenario "A user lists all articles" do
