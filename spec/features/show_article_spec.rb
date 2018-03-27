03b0edd57c834668322467ac8688fde922587f49
Chikara Okada <chikara@gmail.com>
2018-03-27 17:53:14 +0900

    Update spec to verify signed i owners see edit and delete

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -32,6 +32,18 @@ RSpec.feature "Showing an Article" do
     expect(page).not_to have_link("Delete Article")
   end
 
+  scenario "To signed in owners see both edit and delete buttons" do
+    login_as(@john)
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
+
   scenario "A user shows an article" do
     visit "/"
     click_link @article.title
