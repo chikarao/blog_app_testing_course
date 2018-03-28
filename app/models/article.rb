e230086ca5154403d9a023267efdd067288b1565
Chikara Okada <chikara@gmail.com>
2018-03-28 11:56:32 +0900

    adding comments implemented

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,6 @@ class Article < ApplicationRecord
   default_scope { order(created_at: :desc) }
   # reorders the recodes in index
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  #deletes related comments  when article destroyed
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+    # creates nested routes /articles/:id/comments/:id
+  end
 end
